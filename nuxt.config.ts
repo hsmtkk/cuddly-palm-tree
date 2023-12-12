d649b999721ce82e3c2d0a4f74e0848bab0141ff
hsmtkk <hsmtkk@gmail.com>
2023-12-12 19:02:02 +0900

    use class less CSS

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,3 +1,11 @@
 export default defineNuxtConfig({
   devtools: { enabled: true },
+  app: {
+    head: {
+      link: [
+        { rel: "stylesheet", href: "https://fonts.xz.style/serve/inter.css" },
+        { rel: 'stylesheet', href: 'https://cdn.jsdelivr.net/npm/@exampledev/new.css@1.1.2/new.min.css' },
+      ]
+    }
+  },
 })
